3d5afd7f7851d59c3d11db419bd3aa2073009fd9
Ishara sewwandi <153321204+Ishara-Sewwandi@users.noreply.github.com>
2024-05-11 16:33:28 +0530

    changes did in first 2pages and set navigation

--- app/src/main/java/com/example/doit/MainActivity.kt
+++ app/src/main/java/com/example/doit/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.doit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.doit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // Declare the binding variable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addBtn.setOnClickListener {
+            val intent = Intent(this,AddTask::class.java)
+            startActivity(intent)
+        }
     }
 }
