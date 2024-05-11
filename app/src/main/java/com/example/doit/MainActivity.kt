835841f153aecaec7acfca78ff4d2cc627244463
Ishara sewwandi <153321204+Ishara-Sewwandi@users.noreply.github.com>
2024-05-12 01:34:26 +0530

    implement read functionality

--- app/src/main/java/com/example/doit/MainActivity.kt
+++ app/src/main/java/com/example/doit/MainActivity.kt
@@ -3,19 +3,33 @@ package com.example.doit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.doit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding // Declare the binding variable
+    private lateinit var db: TaskDatabase
+    private lateinit var tasksAdapter: TasksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = TaskDatabase(this)
+        tasksAdapter = TasksAdapter(this,db.getAllTasks())
+
+        binding.taskRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.taskRecyclerView.adapter =tasksAdapter
+
         binding.addBtn.setOnClickListener {
             val intent = Intent(this,AddTask::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tasksAdapter.refreshData(db.getAllTasks())
     }
 }
