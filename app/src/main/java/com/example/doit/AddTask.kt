a85af67fade6ae67832eab4c5e3718ce6f5131a3
Ishara sewwandi <153321204+Ishara-Sewwandi@users.noreply.github.com>
2024-05-11 19:49:01 +0530

    create function done

--- app/src/main/java/com/example/doit/AddTask.kt
+++ app/src/main/java/com/example/doit/AddTask.kt
@@ -1,11 +1,34 @@
 package com.example.doit
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.doit.databinding.ActivityAddTaskBinding
+import com.example.doit.databinding.ActivityMainBinding
 
 class AddTask : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityAddTaskBinding
+    private lateinit var db: TaskDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDatabase(this)
+
+//        getting user inputs
+        binding.addsavebtn.setOnClickListener{
+            val title = binding.editTitle.text.toString()
+            val content = binding.editContent.text.toString()
+            val task = Task(0,title,content)
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this,"Task saved",Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 }
